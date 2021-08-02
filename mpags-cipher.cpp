f958af71b59b6f15930734f2da9e237b85f8254f
Thomas Latham <T.Latham@warwick.ac.uk>
2021-08-02 15:58:13 +0000

    Add transliteration code to main function - Ex. 5

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,10 +1,58 @@
+#include <cctype>
 #include <iostream>
+#include <string>
 
 int main()
 {
-    // This is a comment
-    /* This is a multi-
-    line comment */
+    // Initialise variables
+    char c{'x'};
+    std::string out_text;
 
-    std::cout << "Hello world!\n";
+    // loop over each character from user input
+    while (std::cin >> c) {
+        // Uppercase alphabetic characters
+        if (std::isalpha(c)) {
+            out_text += std::toupper(c);
+            continue;
+        }
+
+        // Transliterate digits to English words
+        switch (c) {
+            case '0':
+                out_text += "ZERO";
+                break;
+            case '1':
+                out_text += "ONE";
+                break;
+            case '2':
+                out_text += "TWO";
+                break;
+            case '3':
+                out_text += "THREE";
+                break;
+            case '4':
+                out_text += "FOUR";
+                break;
+            case '5':
+                out_text += "FIVE";
+                break;
+            case '6':
+                out_text += "SIX";
+                break;
+            case '7':
+                out_text += "SEVEN";
+                break;
+            case '8':
+                out_text += "EIGHT";
+                break;
+            case '9':
+                out_text += "NINE";
+                break;
+        }
+
+        // If the character isn't alphabetic or numeric, DONT add it
+    }
+
+    // Print out the transliterated text
+    std::cout << out_text << std::endl;
 }
