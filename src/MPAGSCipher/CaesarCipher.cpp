ecb39e16e7609d1aec7ddf1e9de2b7bb10e4622b
Thomas Latham <T.Latham@warwick.ac.uk>
2021-08-03 15:41:25 +0000

    Add 2nd constructor for CaesarCipher class - Ex. 3

--- src/MPAGSCipher/CaesarCipher.cpp
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -1,5 +1,37 @@
 #include "CaesarCipher.hpp"
 
+#include <iostream>
+#include <string>
+
 CaesarCipher::CaesarCipher(const std::size_t key) : key_{key}
 {
+}
+
+CaesarCipher::CaesarCipher(const std::string& key) : key_{0}
+{
+    // We have the key as a string, but the Caesar cipher needs an unsigned long, so we first need to convert it
+    // We default to having a key of 0, i.e. no encryption, if no (valid) key was provided on the command line
+    if (!key.empty()) {
+        // Before doing the conversion we should check that the string contains a
+        // valid positive integer.
+        // Here we do that by looping through each character and checking that it
+        // is a digit. What is rather hard to check is whether the number is too
+        // large to be represented by an unsigned long, so we've omitted that for
+        // the time being.
+        // (Since the conversion function std::stoul will throw an exception if the
+        // string does not represent a valid unsigned long, we could check for and
+        // handle that instead but we only cover exceptions very briefly on the
+        // final day of this course - they are a very complex area of C++ that
+        // could take an entire course on their own!)
+        for (const auto& elem : key) {
+            if (!std::isdigit(elem)) {
+                std::cerr
+                    << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                    << "        the supplied key (" << key
+                    << ") could not be successfully converted" << std::endl;
+                return;
+            }
+        }
+        key_ = std::stoul(key);
+    }
 }
--- src/MPAGSCipher/CaesarCipher.hpp
+++ src/MPAGSCipher/CaesarCipher.hpp
@@ -2,11 +2,16 @@
 #define MPAGSCIPHER_CAESARCIPHER_HPP
 
 #include <cstddef>
+#include <string>
 
 /// Encrypt or decrypt text using the Caesar cipher with the given key
 class CaesarCipher {
   public:
-    CaesarCipher(const std::size_t key);
+    /// Create a new CaesarCipher, with the given key
+    explicit CaesarCipher(const std::size_t key);
+
+    /// Create a new CaesarCipher, converting the given string into the key
+    explicit CaesarCipher(const std::string& key);
 
     std::size_t key_{0};
 };
