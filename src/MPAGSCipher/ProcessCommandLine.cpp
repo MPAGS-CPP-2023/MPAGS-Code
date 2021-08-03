f4e23f28644c6660663ffc266e90b65e6aae9033
Thomas Latham <T.Latham@warwick.ac.uk>
2021-08-03 12:53:48 +0000

    Modularised functions and added CMake support - CMake walkthrough

---
+++ src/MPAGSCipher/ProcessCommandLine.cpp
@@ -0,0 +1,64 @@
+#include "ProcessCommandLine.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool processCommandLine(const std::vector<std::string>& cmdLineArgs,
+                        bool& helpRequested, bool& versionRequested,
+                        std::string& inputFile, std::string& outputFile)
+{
+    // Status flag to indicate whether or not the parsing was successful
+    bool processStatus{true};
+
+    // Process the arguments - ignore zeroth element, as we know this to be
+    // the program name and don't need to worry about it
+    const std::size_t nCmdLineArgs{cmdLineArgs.size()};
+    for (std::size_t i{1}; i < nCmdLineArgs; ++i) {
+        if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
+            // Set the indicator and terminate the loop
+            helpRequested = true;
+            break;
+        } else if (cmdLineArgs[i] == "--version") {
+            // Set the indicator and terminate the loop
+            versionRequested = true;
+            break;
+        } else if (cmdLineArgs[i] == "-i") {
+            // Handle input file option
+            // Next element is filename unless "-i" is the last argument
+            if (i == nCmdLineArgs - 1) {
+                std::cerr << "[error] -i requires a filename argument"
+                          << std::endl;
+                // Set the flag to indicate the error and terminate the loop
+                processStatus = false;
+                break;
+            } else {
+                // Got filename, so assign value and advance past it
+                inputFile = cmdLineArgs[i + 1];
+                ++i;
+            }
+        } else if (cmdLineArgs[i] == "-o") {
+            // Handle output file option
+            // Next element is filename unless "-o" is the last argument
+            if (i == nCmdLineArgs - 1) {
+                std::cerr << "[error] -o requires a filename argument"
+                          << std::endl;
+                // Set the flag to indicate the error and terminate the loop
+                processStatus = false;
+                break;
+            } else {
+                // Got filename, so assign value and advance past it
+                outputFile = cmdLineArgs[i + 1];
+                ++i;
+            }
+        } else {
+            // Have encoutered an unknown flag, output an error message,
+            // set the flag to indicate the error and terminate the loop
+            std::cerr << "[error] unknown argument '" << cmdLineArgs[i]
+                      << "'\n";
+            processStatus = false;
+            break;
+        }
+    }
+    return processStatus;
+}
---
+++ src/MPAGSCipher/ProcessCommandLine.hpp
@@ -0,0 +1,11 @@
+#ifndef MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+#define MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+
+#include <string>
+#include <vector>
+
+bool processCommandLine(const std::vector<std::string>& cmdLineArgs,
+                        bool& helpRequested, bool& versionRequested,
+                        std::string& inputFile, std::string& outputFile);
+
+#endif    // MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
