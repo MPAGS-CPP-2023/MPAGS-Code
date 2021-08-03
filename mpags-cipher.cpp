5faddbc99aeac9e548270fd5f30b05905734ecf0
Thomas Latham <T.Latham@warwick.ac.uk>
2021-08-03 08:36:05 +0000

    Implement remaining cmd-line args - Ex. 8

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -11,12 +11,49 @@ int main(int argc, char* argv[])
 
     // Options that might be set by the command-line arguments
     bool helpRequested{false};
+    bool versionRequested{false};
+    std::string inputFile{""};
+    std::string outputFile{""};
 
     // Process command line arguments - ignore zeroth element, as we know this
     // to be the program name and don't need to worry about it
     for (std::size_t i{1}; i < nCmdLineArgs; ++i) {
         if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
             helpRequested = true;
+        } else if (cmdLineArgs[i] == "--version") {
+            versionRequested = true;
+        } else if (cmdLineArgs[i] == "-i") {
+            // Handle input file option
+            // Next element is filename unless "-i" is the last argument
+            if (i == nCmdLineArgs - 1) {
+                std::cerr << "[error] -i requires a filename argument"
+                          << std::endl;
+                // exit main with non-zero return to indicate failure
+                return 1;
+            } else {
+                // Got filename, so assign value and advance past it
+                inputFile = cmdLineArgs[i + 1];
+                ++i;
+            }
+        } else if (cmdLineArgs[i] == "-o") {
+            // Handle output file option
+            // Next element is filename unless "-o" is the last argument
+            if (i == nCmdLineArgs - 1) {
+                std::cerr << "[error] -o requires a filename argument"
+                          << std::endl;
+                // exit main with non-zero return to indicate failure
+                return 1;
+            } else {
+                // Got filename, so assign value and advance past it
+                outputFile = cmdLineArgs[i + 1];
+                ++i;
+            }
+        } else {
+            // Have an unknown flag to output error message and return non-zero
+            // exit status to indicate failure
+            std::cerr << "[error] unknown argument '" << cmdLineArgs[i]
+                      << "'\n";
+            return 1;
         }
     }
 
@@ -24,59 +61,79 @@ int main(int argc, char* argv[])
     if (helpRequested) {
         // Line splitting for readability
         std::cout
-            << "Usage: mpags-cipher [-h/--help]\n\n"
+            << "Usage: mpags-cipher [-h/--help] [--version] [-i <file>] [-o <file>]\n\n"
             << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
             << "Available options:\n\n"
             << "  -h|--help        Print this help message and exit\n\n"
+            << "  --version        Print version information\n\n"
+            << "  -i FILE          Read text to be processed from FILE\n"
+            << "                   Stdin will be used if not supplied\n\n"
+            << "  -o FILE          Write processed text to FILE\n"
+            << "                   Stdout will be used if not supplied\n\n"
             << std::endl;
         // Help requires no further action, so return from main
         // with 0 used to indicate success
         return 0;
     }
 
+    // Handle version, if requested
+    // Like help, requires no further action,
+    // so return from main with zero to indicate success
+    if (versionRequested) {
+        std::cout << "0.1.0" << std::endl;
+        return 0;
+    }
+
     // Initialise variables
-    char c{'x'};
-    std::string out_text;
+    char inputChar{'x'};
+    std::string inputText;
+
+    // Read in user input from stdin/file
+    // Warn that input file option not yet implemented
+    if (!inputFile.empty()) {
+        std::cerr << "[warning] input from file ('" << inputFile
+                  << "') not implemented yet, using stdin\n";
+    }
 
     // loop over each character from user input
-    while (std::cin >> c) {
+    while (std::cin >> inputChar) {
         // Uppercase alphabetic characters
-        if (std::isalpha(c)) {
-            out_text += std::toupper(c);
+        if (std::isalpha(inputChar)) {
+            inputText += std::toupper(inputChar);
             continue;
         }
 
         // Transliterate digits to English words
-        switch (c) {
+        switch (inputChar) {
             case '0':
-                out_text += "ZERO";
+                inputText += "ZERO";
                 break;
             case '1':
-                out_text += "ONE";
+                inputText += "ONE";
                 break;
             case '2':
-                out_text += "TWO";
+                inputText += "TWO";
                 break;
             case '3':
-                out_text += "THREE";
+                inputText += "THREE";
                 break;
             case '4':
-                out_text += "FOUR";
+                inputText += "FOUR";
                 break;
             case '5':
-                out_text += "FIVE";
+                inputText += "FIVE";
                 break;
             case '6':
-                out_text += "SIX";
+                inputText += "SIX";
                 break;
             case '7':
-                out_text += "SEVEN";
+                inputText += "SEVEN";
                 break;
             case '8':
-                out_text += "EIGHT";
+                inputText += "EIGHT";
                 break;
             case '9':
-                out_text += "NINE";
+                inputText += "NINE";
                 break;
         }
 
@@ -84,7 +141,14 @@ int main(int argc, char* argv[])
     }
 
     // Print out the transliterated text
-    std::cout << out_text << std::endl;
+
+    // Warn that output file option not yet implemented
+    if (!outputFile.empty()) {
+        std::cerr << "[warning] output to file ('" << outputFile
+                  << "') not implemented yet, using stdout\n";
+    }
+
+    std::cout << inputText << std::endl;
 
     // No requirement to return from main, but we do so for clarity
     // and for consistency with other functions
